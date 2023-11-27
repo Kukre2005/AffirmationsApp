05bc232440f47bb7854abe8a377815ad1c2a7c5b
kukre2005 <ariasmanuel191@gmail.com>
2023-11-27 06:54:32 -0300

    Affirmations App V1.2 (Completada la insercion de la lista con las imagenes)

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -24,6 +24,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -36,6 +38,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
 import com.example.affirmations.ui.theme.AffirmationsTheme
 import com.example.affirmations.model.Affirmations
 
@@ -60,6 +63,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AffirmationsApp() {
+    AffirmationsList(
+        affirmationsList = Datasource().loadAffirmations(),
+    )
 }
 
 @Composable
@@ -84,8 +90,15 @@ fun AffirmationsCard(affirmations: Affirmations, modifier: Modifier = Modifier) 
 }
 
 @Composable
-fun AffirmationsList(affirmations: List<Affirmations>){
-
+fun AffirmationsList(affirmationsList: List<Affirmations>, modifier: Modifier = Modifier){
+    LazyColumn(modifier = modifier){
+        items(affirmationsList){ affirmations ->
+            AffirmationsCard(
+                affirmations = affirmations,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
 }
 
 @Preview
